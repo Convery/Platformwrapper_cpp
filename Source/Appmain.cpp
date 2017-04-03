5d8b62de3243ec5c7a7cfc6f710f45cccd6e5bdf
Convery <convery10122@gmail.com>
2017-04-03 04:22:39 +0200

    Added steams IAT modification to Appmain

--- Source/Appmain.cpp
+++ Source/Appmain.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "StdInclude.h"
+#include "Steam/Steam.h"
 
 // Clear the logfile on startup so we only save this session.
 namespace { struct Deletelog { Deletelog() { Clearlogfile(); } }; static Deletelog Deleted{}; }
@@ -18,13 +19,64 @@ extern "C"
         if (Initialized) return;
         else Initialized = true;
         
-        /*
-            Usage:
-            ----------------------------------------------------------------------
-            This callback is called at the games entrypoint, which means that all
-            other libraries are initialized. Your plugin should make all the .text
-            segment modifications from this callback. Also your initialization.
-        */
+        constexpr const char *Steamdll = sizeof(size_t) == sizeof(uint64_t) ? "steam_api64.dll" : "steam_api.dll";
+
+        #define PATCH_STEAM_IAT(Function)                                           \
+        size_t Address ##Function = GetIATFunction(Steamdll, #Function);            \
+        if(Address ##Function) *(size_t *)Address ##Function = size_t(Function);    \
+
+        // Steamworks exports.
+        {
+            PATCH_STEAM_IAT(SteamAppList);
+            PATCH_STEAM_IAT(SteamApps);
+            PATCH_STEAM_IAT(SteamClient);
+            PATCH_STEAM_IAT(SteamController);
+            PATCH_STEAM_IAT(SteamFriends);
+            PATCH_STEAM_IAT(SteamGameServer);
+            PATCH_STEAM_IAT(SteamGameServerHTTP);
+            PATCH_STEAM_IAT(SteamGameServerInventory);
+            PATCH_STEAM_IAT(SteamGameServerNetworking);
+            PATCH_STEAM_IAT(SteamGameServerStats);
+            PATCH_STEAM_IAT(SteamGameServerUGC);
+            PATCH_STEAM_IAT(SteamGameServerUtils);
+            PATCH_STEAM_IAT(SteamHTMLSurface);
+            PATCH_STEAM_IAT(SteamHTTP);
+            PATCH_STEAM_IAT(SteamInventory);
+            PATCH_STEAM_IAT(SteamMatchmaking);
+            PATCH_STEAM_IAT(SteamMatchmakingServers);
+            PATCH_STEAM_IAT(SteamMusic);
+            PATCH_STEAM_IAT(SteamMusicRemote);
+            PATCH_STEAM_IAT(SteamNetworking);
+            PATCH_STEAM_IAT(SteamRemoteStorage);
+            PATCH_STEAM_IAT(SteamScreenshots);
+            PATCH_STEAM_IAT(SteamUnifiedMessages);
+            PATCH_STEAM_IAT(SteamUGC);
+            PATCH_STEAM_IAT(SteamUser);
+            PATCH_STEAM_IAT(SteamUserStats);
+            PATCH_STEAM_IAT(SteamUtils);
+            PATCH_STEAM_IAT(SteamVideo);
+            PATCH_STEAM_IAT(SteamMasterServerUpdater);
+
+            PATCH_STEAM_IAT(SteamAPI_Init);
+            PATCH_STEAM_IAT(SteamAPI_InitSafe);
+            PATCH_STEAM_IAT(SteamAPI_Shutdown);
+            PATCH_STEAM_IAT(SteamAPI_IsSteamRunning);
+            PATCH_STEAM_IAT(SteamAPI_GetSteamInstallPath);
+            PATCH_STEAM_IAT(SteamAPI_RestartAppIfNecessary);
+
+            PATCH_STEAM_IAT(SteamAPI_RunCallbacks);
+            PATCH_STEAM_IAT(SteamAPI_RegisterCallback);
+            PATCH_STEAM_IAT(SteamAPI_UnregisterCallback);
+            PATCH_STEAM_IAT(SteamAPI_RegisterCallResult);
+            PATCH_STEAM_IAT(SteamAPI_UnregisterCallResult);
+
+            PATCH_STEAM_IAT(SteamGameServer_BSecure);
+            PATCH_STEAM_IAT(SteamGameServer_Shutdown);
+            PATCH_STEAM_IAT(SteamGameServer_RunCallbacks);
+            PATCH_STEAM_IAT(SteamGameServer_GetSteamID);
+            PATCH_STEAM_IAT(SteamGameServer_Init);
+            PATCH_STEAM_IAT(SteamGameServer_InitSafe);
+        }
     }
     EXPORT_ATTR void __cdecl onInitializationDone(bool Reserved)
     {
