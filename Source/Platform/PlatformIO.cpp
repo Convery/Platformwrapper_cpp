29ed6e661a61e4648fef8d63b7efb32e627263d2
Convery <convery10122@gmail.com>
2018-02-25 04:16:24 +0100

    Added length-checking to PlatformIO

--- Source/Platform/PlatformIO.cpp
+++ Source/Platform/PlatformIO.cpp
@@ -44,7 +44,7 @@ namespace Platform
             // 'Deserialize'
             char JSON[2048]{};
             uint32_t MSGType{};
-            std::sscanf(Buffer, "%u|%s", &MSGType, JSON);
+            std::sscanf(Buffer, "%u|%2048s", &MSGType, JSON);
 
             // Check if it's the one we expect, else OOB.
             if (MSGType != uint32_t(Type + 1))
@@ -73,7 +73,7 @@ namespace Platform
         if (!Pipe) return Result;
 
         // Format the message so we can do a single send.
-        auto Formatted = va("%u|%s", Type, Data.c_str());
+        auto Formatted = va("%u|%2048s", Type, Data.c_str());
         write(Pipe, Formatted.c_str(), (uint32_t)Formatted.size());
 
         // Poll until we get our response.
