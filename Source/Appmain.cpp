de6b9581babb0ef6db95c54ce6ffe50eabf125f1
Convery <convery10122@gmail.com>
2018-04-19 04:34:26 +0200

    Made IAT hooking the default mode (formerly STOMP).

--- Source/Appmain.cpp
+++ Source/Appmain.cpp
@@ -11,112 +11,135 @@
 // Callbacks from the bootstrapper (Bootstrapmodule_cpp).
 extern "C" EXPORT_ATTR void onInitializationStart(bool Reserved)
 {
-    // Helper macros to save the developers fingers.
-    #if defined(_WIN32)
-    #define GETFUNC(Module, Function) GetProcAddress(GetModuleHandleA(Module), Function)
-    #else
-    #define GETFUNC(Module, Function) dlsym(dlopen(Module, RTLD_LAZY), Function)
-    #endif
-    #define INSTALL_HOOK(Module, Import, Replacement)                       \
-    {auto Address = GETFUNC(Module, Import);                                \
-    if(Address) Hooking::Stomphook<>::Install(Address, Replacement);}       \
+    // Helpers rather than the old macros.
+    auto Commandlinecontains = [](std::string_view Needle) -> bool
+    {
+        #if defined (_WIN32)
+        return !!std::strstr(GetCommandLineA(), Needle.data());
+        #else
+        std::FILE *Filehandle = std::fopen("/proc/self/cmdline", "r");
+        char Buffer[1024]{};
+        if (Filehandle)
+        {
+            std::fgets(Buffer, 1024, Filehandle);
+            std::fclose(Filehandle);
+        }
+        return !!std::strstr(Buffer, Needle.data());
+        #endif
+    };
+    auto InstallSTOMP = [](std::string_view Modulename, std::string_view Functionname, void *Target) -> void
+    {
+        #if defined(_WIN32)
+        auto Address = GetProcAddress(LoadLibraryA(Modulename.data()), Functionname.data());
+        #else
+        auto Address = dlsym(dlopen(Modulename.data(), RTLD_LAZY), Functionname.data());
+        #endif
 
-    /*
-        TODO(Convery):
-        Make it optional to install these hooks by patching
-        the applications IAT rather than stomphooking the
-        loaded modules. Although STMSIG_Bypass and other
-        such plugins would make modifications to the
-        modules somewhat irrelevant.
-    */
+        if(Address) Hooking::Stomphook<>::Install(Address, Target);
+    };
+    auto InstallIAT = [](std::string_view Modulename, std::string_view Functionname, void *Target) -> void
+    {
+        auto Address = Pattern::Findimportaddress(Modulename, Functionname);
+        if (Address) *(size_t *)Address = (size_t)Target;
+    };
+    std::function<void(std::string_view, std::string_view, void *)> Installer{ InstallIAT };
 
-    // Steam_api exports.
-    #if defined(_WIN32)
-    constexpr const char *Steamlib = sizeof(void *) == sizeof(uint64_t) ? "steam_api64.dll" : "steam_api.dll";
-    #else
-    constexpr const char *Steamlib = sizeof(void *) == sizeof(uint64_t) ? "libsteam_api64.so" : "libsteam_api.so";
-    #endif
-    INSTALL_HOOK(Steamlib, "SteamAPI_RestartAppIfNecessary", SteamAPI_RestartAppIfNecessary);
-    INSTALL_HOOK(Steamlib, "SteamInternal_CreateInterface", SteamInternal_CreateInterface);
-    INSTALL_HOOK(Steamlib, "SteamGameServer_GetHSteamUser", SteamGameServer_GetHSteamUser);
-    INSTALL_HOOK(Steamlib, "SteamGameServer_GetHSteamPipe", SteamGameServer_GetHSteamPipe);
-    INSTALL_HOOK(Steamlib, "SteamAPI_UnregisterCallResult", SteamAPI_UnregisterCallResult);
-    INSTALL_HOOK(Steamlib, "SteamInternal_GameServer_Init", SteamInternal_GameServer_Init);
-    INSTALL_HOOK(Steamlib, "SteamAPI_GetSteamInstallPath", SteamAPI_GetSteamInstallPath);
-    INSTALL_HOOK(Steamlib, "SteamGameServer_RunCallbacks", SteamGameServer_RunCallbacks);
-    INSTALL_HOOK(Steamlib, "SteamAPI_UnregisterCallback", SteamAPI_UnregisterCallback);
-    INSTALL_HOOK(Steamlib, "SteamAPI_RegisterCallResult", SteamAPI_RegisterCallResult);
-    INSTALL_HOOK(Steamlib, "SteamGameServer_GetSteamID", SteamGameServer_GetSteamID);
-    INSTALL_HOOK(Steamlib, "SteamAPI_RegisterCallback", SteamAPI_RegisterCallback);
-    INSTALL_HOOK(Steamlib, "SteamGameServerNetworking", SteamGameServerNetworking);
-    INSTALL_HOOK(Steamlib, "SteamGameServer_InitSafe", SteamGameServer_InitSafe);
-    INSTALL_HOOK(Steamlib, "SteamGameServerInventory", SteamGameServerInventory);
-    INSTALL_HOOK(Steamlib, "SteamMasterServerUpdater", SteamMasterServerUpdater);
-    INSTALL_HOOK(Steamlib, "SteamGameServer_Shutdown", SteamGameServer_Shutdown);
-    INSTALL_HOOK(Steamlib, "SteamGameServer_BSecure", SteamGameServer_BSecure);
-    INSTALL_HOOK(Steamlib, "SteamMatchmakingServers", SteamMatchmakingServers);
-    INSTALL_HOOK(Steamlib, "SteamAPI_IsSteamRunning", SteamAPI_IsSteamRunning);
-    INSTALL_HOOK(Steamlib, "SteamAPI_GetHSteamUser", SteamAPI_GetHSteamUser);
-    INSTALL_HOOK(Steamlib, "SteamAPI_GetHSteamPipe", SteamAPI_GetHSteamPipe);
-    INSTALL_HOOK(Steamlib, "SteamAPI_RunCallbacks", SteamAPI_RunCallbacks);
-    INSTALL_HOOK(Steamlib, "SteamUnifiedMessages", SteamUnifiedMessages);
-    INSTALL_HOOK(Steamlib, "SteamGameServerStats", SteamGameServerStats);
-    INSTALL_HOOK(Steamlib, "SteamGameServerUtils", SteamGameServerUtils);
-    INSTALL_HOOK(Steamlib, "SteamGameServer_Init", SteamGameServer_Init);
-    INSTALL_HOOK(Steamlib, "SteamGameServerHTTP", SteamGameServerHTTP);
-    INSTALL_HOOK(Steamlib, "SteamGameServerUGC", SteamGameServerUGC);
-    INSTALL_HOOK(Steamlib, "SteamRemoteStorage", SteamRemoteStorage);
-    INSTALL_HOOK(Steamlib, "SteamAPI_InitSafe", SteamAPI_InitSafe);
-    INSTALL_HOOK(Steamlib, "SteamAPI_Shutdown", SteamAPI_Shutdown);
-    INSTALL_HOOK(Steamlib, "SteamHTMLSurface", SteamHTMLSurface);
-    INSTALL_HOOK(Steamlib, "SteamScreenshots", SteamScreenshots);
-    INSTALL_HOOK(Steamlib, "SteamMusicRemote", SteamMusicRemote);
-    INSTALL_HOOK(Steamlib, "SteamMatchmaking", SteamMatchmaking);
-    INSTALL_HOOK(Steamlib, "SteamNetworking", SteamNetworking);
-    INSTALL_HOOK(Steamlib, "SteamController", SteamController);
-    INSTALL_HOOK(Steamlib, "SteamGameServer", SteamGameServer);
-    INSTALL_HOOK(Steamlib, "SteamInventory", SteamInventory);
-    INSTALL_HOOK(Steamlib, "SteamUserStats", SteamUserStats);
-    INSTALL_HOOK(Steamlib, "SteamAPI_Init", SteamAPI_Init);
-    INSTALL_HOOK(Steamlib, "SteamAppList", SteamAppList);
-    INSTALL_HOOK(Steamlib, "SteamFriends", SteamFriends);
-    INSTALL_HOOK(Steamlib, "SteamClient", SteamClient);
-    INSTALL_HOOK(Steamlib, "SteamMusic", SteamMusic);
-    INSTALL_HOOK(Steamlib, "SteamUtils", SteamUtils);
-    INSTALL_HOOK(Steamlib, "SteamVideo", SteamVideo);
-    INSTALL_HOOK(Steamlib, "SteamApps", SteamApps);
-    INSTALL_HOOK(Steamlib, "SteamUser", SteamUser);
-    INSTALL_HOOK(Steamlib, "SteamHTTP", SteamHTTP);
-    INSTALL_HOOK(Steamlib, "SteamUGC", SteamUGC);
-
-    // Arclight exports.
+    // The modules that should be handled.
     #if defined(_WIN32)
     constexpr const char *Arclib = "ArcSDK.dll";
+    constexpr const char *Steamlib = sizeof(void *) == sizeof(uint64_t) ? "steam_api64.dll" : "steam_api.dll";
     #else
     constexpr const char *Arclib = ""; // TODO(Convery): Investigate this.
+    constexpr const char *Steamlib = sizeof(void *) == sizeof(uint64_t) ? "libsteam_api64.so" : "libsteam_api.so";
     #endif
-    INSTALL_HOOK(Arclib, "CC_InviteFriendInOverlay", CC_InviteFriendInOverlay);
-    INSTALL_HOOK(Arclib, "CC_GetLaunchedParameter", CC_GetLaunchedParameter);
-    INSTALL_HOOK(Arclib, "CC_UnregisterCallback", CC_UnregisterCallback);
-    INSTALL_HOOK(Arclib, "CC_GetArcRunningMode", CC_GetArcRunningMode);
-    INSTALL_HOOK(Arclib, "CC_GetUserAvatarLink", CC_GetUserAvatarLink);
-    INSTALL_HOOK(Arclib, "CC_RegisterCallback", CC_RegisterCallback);
-    INSTALL_HOOK(Arclib, "CC_InstalledFromArc", CC_InstalledFromArc);
-    INSTALL_HOOK(Arclib, "CC_GotoUrlInOverlay", CC_GotoUrlInOverlay);
-    INSTALL_HOOK(Arclib, "CC_GetSteamTicket", CC_GetSteamTicket);
-    INSTALL_HOOK(Arclib, "CC_GetAccountName", CC_GetAccountName);
-    INSTALL_HOOK(Arclib, "CC_LaunchClient", CC_LaunchClient);
-    INSTALL_HOOK(Arclib, "CC_RunCallbacks", CC_RunCallbacks);
-    INSTALL_HOOK(Arclib, "CC_GetNickName", CC_GetNickName);
-    INSTALL_HOOK(Arclib, "CC_ShowOverlay", CC_ShowOverlay);
-    INSTALL_HOOK(Arclib, "CC_GetTokenEx", CC_GetTokenEx);
-    INSTALL_HOOK(Arclib, "CC_GetToken", CC_GetToken);
-    INSTALL_HOOK(Arclib, "CC_GetArcID", CC_GetArcID);
-    INSTALL_HOOK(Arclib, "Matchmaking", Matchmaking);
-    INSTALL_HOOK(Arclib, "Networking", Networking);
-    INSTALL_HOOK(Arclib, "ArcFriends", ArcFriends);
-    INSTALL_HOOK(Arclib, "CC_UnInit", CC_UnInit);
-    INSTALL_HOOK(Arclib, "CC_Init", CC_Init);
+
+    // Check if we should prefer stomp-hooking.
+    if (Commandlinecontains("-stomp"))
+    {
+        Installer = InstallSTOMP;
+    }
+
+    // Hook steams exports.
+    #pragma region STEAM
+    Installer(Steamlib, "SteamAPI_RestartAppIfNecessary", SteamAPI_RestartAppIfNecessary);
+    Installer(Steamlib, "SteamInternal_CreateInterface", SteamInternal_CreateInterface);
+    Installer(Steamlib, "SteamGameServer_GetHSteamUser", SteamGameServer_GetHSteamUser);
+    Installer(Steamlib, "SteamGameServer_GetHSteamPipe", SteamGameServer_GetHSteamPipe);
+    Installer(Steamlib, "SteamAPI_UnregisterCallResult", SteamAPI_UnregisterCallResult);
+    Installer(Steamlib, "SteamInternal_GameServer_Init", SteamInternal_GameServer_Init);
+    Installer(Steamlib, "SteamAPI_GetSteamInstallPath", SteamAPI_GetSteamInstallPath);
+    Installer(Steamlib, "SteamGameServer_RunCallbacks", SteamGameServer_RunCallbacks);
+    Installer(Steamlib, "SteamAPI_UnregisterCallback", SteamAPI_UnregisterCallback);
+    Installer(Steamlib, "SteamAPI_RegisterCallResult", SteamAPI_RegisterCallResult);
+    Installer(Steamlib, "SteamGameServer_GetSteamID", SteamGameServer_GetSteamID);
+    Installer(Steamlib, "SteamAPI_RegisterCallback", SteamAPI_RegisterCallback);
+    Installer(Steamlib, "SteamGameServerNetworking", SteamGameServerNetworking);
+    Installer(Steamlib, "SteamGameServer_InitSafe", SteamGameServer_InitSafe);
+    Installer(Steamlib, "SteamGameServerInventory", SteamGameServerInventory);
+    Installer(Steamlib, "SteamMasterServerUpdater", SteamMasterServerUpdater);
+    Installer(Steamlib, "SteamGameServer_Shutdown", SteamGameServer_Shutdown);
+    Installer(Steamlib, "SteamGameServer_BSecure", SteamGameServer_BSecure);
+    Installer(Steamlib, "SteamMatchmakingServers", SteamMatchmakingServers);
+    Installer(Steamlib, "SteamAPI_IsSteamRunning", SteamAPI_IsSteamRunning);
+    Installer(Steamlib, "SteamAPI_GetHSteamUser", SteamAPI_GetHSteamUser);
+    Installer(Steamlib, "SteamAPI_GetHSteamPipe", SteamAPI_GetHSteamPipe);
+    Installer(Steamlib, "SteamAPI_RunCallbacks", SteamAPI_RunCallbacks);
+    Installer(Steamlib, "SteamUnifiedMessages", SteamUnifiedMessages);
+    Installer(Steamlib, "SteamGameServerStats", SteamGameServerStats);
+    Installer(Steamlib, "SteamGameServerUtils", SteamGameServerUtils);
+    Installer(Steamlib, "SteamGameServer_Init", SteamGameServer_Init);
+    Installer(Steamlib, "SteamGameServerHTTP", SteamGameServerHTTP);
+    Installer(Steamlib, "SteamGameServerUGC", SteamGameServerUGC);
+    Installer(Steamlib, "SteamRemoteStorage", SteamRemoteStorage);
+    Installer(Steamlib, "SteamAPI_InitSafe", SteamAPI_InitSafe);
+    Installer(Steamlib, "SteamAPI_Shutdown", SteamAPI_Shutdown);
+    Installer(Steamlib, "SteamHTMLSurface", SteamHTMLSurface);
+    Installer(Steamlib, "SteamScreenshots", SteamScreenshots);
+    Installer(Steamlib, "SteamMusicRemote", SteamMusicRemote);
+    Installer(Steamlib, "SteamMatchmaking", SteamMatchmaking);
+    Installer(Steamlib, "SteamNetworking", SteamNetworking);
+    Installer(Steamlib, "SteamController", SteamController);
+    Installer(Steamlib, "SteamGameServer", SteamGameServer);
+    Installer(Steamlib, "SteamInventory", SteamInventory);
+    Installer(Steamlib, "SteamUserStats", SteamUserStats);
+    Installer(Steamlib, "SteamAPI_Init", SteamAPI_Init);
+    Installer(Steamlib, "SteamAppList", SteamAppList);
+    Installer(Steamlib, "SteamFriends", SteamFriends);
+    Installer(Steamlib, "SteamClient", SteamClient);
+    Installer(Steamlib, "SteamMusic", SteamMusic);
+    Installer(Steamlib, "SteamUtils", SteamUtils);
+    Installer(Steamlib, "SteamVideo", SteamVideo);
+    Installer(Steamlib, "SteamApps", SteamApps);
+    Installer(Steamlib, "SteamUser", SteamUser);
+    Installer(Steamlib, "SteamHTTP", SteamHTTP);
+    Installer(Steamlib, "SteamUGC", SteamUGC);
+    #pragma endregion
+
+    // Hook Arc exports.
+    #pragma region ARC
+    Installer(Arclib, "CC_InviteFriendInOverlay", CC_InviteFriendInOverlay);
+    Installer(Arclib, "CC_GetLaunchedParameter", CC_GetLaunchedParameter);
+    Installer(Arclib, "CC_UnregisterCallback", CC_UnregisterCallback);
+    Installer(Arclib, "CC_GetArcRunningMode", CC_GetArcRunningMode);
+    Installer(Arclib, "CC_GetUserAvatarLink", CC_GetUserAvatarLink);
+    Installer(Arclib, "CC_RegisterCallback", CC_RegisterCallback);
+    Installer(Arclib, "CC_InstalledFromArc", CC_InstalledFromArc);
+    Installer(Arclib, "CC_GotoUrlInOverlay", CC_GotoUrlInOverlay);
+    Installer(Arclib, "CC_GetSteamTicket", CC_GetSteamTicket);
+    Installer(Arclib, "CC_GetAccountName", CC_GetAccountName);
+    Installer(Arclib, "CC_LaunchClient", CC_LaunchClient);
+    Installer(Arclib, "CC_RunCallbacks", CC_RunCallbacks);
+    Installer(Arclib, "CC_GetNickName", CC_GetNickName);
+    Installer(Arclib, "CC_ShowOverlay", CC_ShowOverlay);
+    Installer(Arclib, "CC_GetTokenEx", CC_GetTokenEx);
+    Installer(Arclib, "CC_GetToken", CC_GetToken);
+    Installer(Arclib, "CC_GetArcID", CC_GetArcID);
+    Installer(Arclib, "Matchmaking", Matchmaking);
+    Installer(Arclib, "Networking", Networking);
+    Installer(Arclib, "ArcFriends", ArcFriends);
+    Installer(Arclib, "CC_UnInit", CC_UnInit);
+    Installer(Arclib, "CC_Init", CC_Init);
+    #pragma endregion
 }
 extern "C" EXPORT_ATTR void onInitializationDone(bool Reserved)
 {
